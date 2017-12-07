d09cc46b2a6d981a26b75d4e2e6a71a81463ae6d
max beizer <max.beizer@gmail.com>
2017-12-06 22:46:12 -0600

    Add basic routing

--- src/Models.elm
+++ src/Models.elm
@@ -4,12 +4,19 @@ import RemoteData exposing (WebData)
 
 type alias Model =
     { players : WebData (List Player)
+    , route : Route
     }
 
+type Route
+    = PlayersRoute
+    | PlayerRoute PlayerId
+    | NotFoundRoute
 
-initialModel : Model
-initialModel =
+
+initialModel : Route -> Model
+initialModel route =
     { players = RemoteData.Loading
+    , route = route
     }
 
 
